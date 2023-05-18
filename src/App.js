c9c65b830e4510f620b9d1292b297ab972f939e2
Khaleel-0 <khaleel0@uw.edu>
2023-05-18 06:22:57 -0700

    added frontend files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [data,setData] = useState();
+
+  useEffect(()=>{
+    backendCall();
+  },[])
+
+  const backendCall = () => {
+    fetch('http://127.0.0.1:8000/playground/hello/',{
+      method: 'GET',
+      mode: 'cors',
+    })
+    .then(response => 
+      {response.text();
+      console.log(response)})
+    .then(data => {
+      setData(data);
+      // console.log(data);
+    })
+    .catch(error => console.log(error));
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" 
+        // onClick={backendCall}
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          The response from backend is {data}
         </p>
         <a
           className="App-link"
